7deae390022e7eadf4d79b47f9b7dc051f25145c
qurg <qurg@outlook.com>
2019-06-14 01:39:34 +0800

    change database config.

--- chain/settings.py
+++ chain/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/static/'
+# STATIC_ROOT = 'static'
 
 # 设置用户验证模式
 AUTH_USER_MODEL = 'user.MyUser'
